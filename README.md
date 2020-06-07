060bd8982a3d9e982e62084a25a13c3914b1757f
SmartCoder1011 <65933047+SmartCoder1011@users.noreply.github.com>
2020-06-07 11:48:38 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# C23
-Toppling boxes
+# Box Class
---
+++ ground.js
@@ -0,0 +1,17 @@
+class ground{
+    constructor(x,y,width,height){
+        var ground_options={
+            isStatic:true
+        }
+        this.width=width;
+        this.height=height;
+        this.body=Bodies.rectangle(x,y,width,height,ground_options);
+        World.add(world,this.body);
+    }
+
+    display(){
+        rectMode(CENTER);
+        fill("white");
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+    }
+}
